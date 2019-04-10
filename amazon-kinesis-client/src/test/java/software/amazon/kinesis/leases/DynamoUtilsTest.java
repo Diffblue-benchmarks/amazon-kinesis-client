bf4d52aa0b854b1e99d44769baab869d3515b6f5
James Adam <james.adam@diffblue.com>
2019-04-10 11:56:55 +0100

    Add tests for software.amazon.kinesis.leases.DynamoUtils

--- amazon-kinesis-client/src/test/java/software/amazon/kinesis/leases/DynamoUtilsTest.java
+++ amazon-kinesis-client/src/test/java/software/amazon/kinesis/leases/DynamoUtilsTest.java
@@ -1,212 +1,58 @@
 package software.amazon.kinesis.leases;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.kinesis.leases.DynamoUtils;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DynamoUtilsTest {
 
-  @Rule public ExpectedException thrown = ExpectedException.none();
-
   @Test
-  public void safeGetStringInput1NotNullOutputNull() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put("", null);
-    final String key = "";
-
-    // Act
-    final String retval = DynamoUtils.safeGetString(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetStringInput1NotNullOutputNull2() {
-
-    // Arrange
+  public void testSafeGetString() {
     final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
     dynamoRecord.put(null, null);
-    final String key = "#";
+    Assert.assertNull(DynamoUtils.safeGetString(dynamoRecord, "#"));
 
-    // Act
-    final String retval = DynamoUtils.safeGetString(dynamoRecord, key);
+    final HashMap<String, AttributeValue> dynamoRecord2 = new HashMap<String, AttributeValue>();
+    dynamoRecord2.put("!-\"%", null);
+    Assert.assertNull(DynamoUtils.safeGetString(dynamoRecord2, "!-\"%"));
 
-    // Assert result
-    Assert.assertNull(retval);
+    final HashMap<String, AttributeValue> dynamoRecord3 = new HashMap<String, AttributeValue>();
+    Assert.assertNull(DynamoUtils.safeGetString(dynamoRecord3, ""));
+
+    final HashMap<String, AttributeValue> dynamoRecord4 = new HashMap<String, AttributeValue>();
+    AttributeValue attribute = DynamoUtils.createAttributeValue("test");
+    dynamoRecord4.put("#", attribute);
+    Assert.assertEquals("test", DynamoUtils.safeGetString(dynamoRecord4, "#"));
   }
 
-  @Test
-  public void safeGetStringInput1NotNullOutputNull3() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put("!-\"%", null);
-    final String key = "!-\"%";
-
-    // Act
-    final String retval = DynamoUtils.safeGetString(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
 
   @Test
-  public void safeGetStringInput1NotNullOutputNull6() {
-
-    // Arrange
+  public void testSafeGetSS() {
     final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put(null, null);
-    final String key = "";
-
-    // Act
-    final String retval = DynamoUtils.safeGetString(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetStringInput0NotNullOutputNull() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    final String key = "";
-
-    // Act
-    final String retval = DynamoUtils.safeGetString(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetSSInput1NotNullOutput() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put("!!!!!!!!! ", null);
-    final String key = "!!!!!!!!!";
-
-    // Act
-    final List<String> retval = DynamoUtils.safeGetSS(dynamoRecord, key);
-
-    // Assert result
+    dynamoRecord.put("#", null);
     final ArrayList<String> arrayList = new ArrayList<String>();
-    Assert.assertEquals(arrayList, retval);
+    Assert.assertEquals(arrayList, DynamoUtils.safeGetSS(dynamoRecord, "key"));
+
+    final HashMap<String, AttributeValue> dynamoRecord2 = new HashMap<String, AttributeValue>();
+    Collection<String> collection = new ArrayList();
+    collection.add("test_1");
+    collection.add("test_2");
+    dynamoRecord2.put("#", DynamoUtils.createAttributeValue(collection));
+    Assert.assertEquals(collection, DynamoUtils.safeGetSS(dynamoRecord2, "#"));
   }
 
   @Test
-  public void safeGetSSInput1NotNullOutput2() {
-
-    // Arrange
+  public void testSafeGetLong() {
     final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put(" q c", null);
-    final String key = "ca`c";
+    dynamoRecord.put("#", null);
+    Assert.assertNull(DynamoUtils.safeGetLong(dynamoRecord, "#"));
 
-    // Act
-    final List<String> retval = DynamoUtils.safeGetSS(dynamoRecord, key);
-
-    // Assert result
-    final ArrayList<String> arrayList = new ArrayList<String>();
-    Assert.assertEquals(arrayList, retval);
-  }
-
-  @Test
-  public void safeGetSSInput1NotNullOutput3() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put(null, null);
-    final String key = "";
-
-    // Act
-    final List<String> retval = DynamoUtils.safeGetSS(dynamoRecord, key);
-
-    // Assert result
-    final ArrayList<String> arrayList = new ArrayList<String>();
-    Assert.assertEquals(arrayList, retval);
-  }
-
-  @Test
-  public void safeGetSSInput0NotNullOutput() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    final String key = "";
-
-    // Act
-    final List<String> retval = DynamoUtils.safeGetSS(dynamoRecord, key);
-
-    // Assert result
-    final ArrayList<String> arrayList = new ArrayList<String>();
-    Assert.assertEquals(arrayList, retval);
-  }
-
-  @Test
-  public void safeGetLongInput1NotNullOutputNull() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put("``", null);
-    final String key = "``";
-
-    // Act
-    final Long retval = DynamoUtils.safeGetLong(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetLongInput1NotNullOutputNull2() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    dynamoRecord.put(null, null);
-    final String key = "kk";
-
-    // Act
-    final Long retval = DynamoUtils.safeGetLong(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetLongInput0NotNullOutputNull3() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    final String key = "????????????????";
-
-    // Act
-    final Long retval = DynamoUtils.safeGetLong(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
-  }
-
-  @Test
-  public void safeGetLongInput0NotNullOutputNull() {
-
-    // Arrange
-    final HashMap<String, AttributeValue> dynamoRecord = new HashMap<String, AttributeValue>();
-    final String key = "?";
-
-    // Act
-    final Long retval = DynamoUtils.safeGetLong(dynamoRecord, key);
-
-    // Assert result
-    Assert.assertNull(retval);
+    final HashMap<String, AttributeValue> dynamoRecord2 = new HashMap<String, AttributeValue>();
+    dynamoRecord2.put("#", DynamoUtils.createAttributeValue(12L));
+    Assert.assertEquals(Long.valueOf(12), DynamoUtils.safeGetLong(dynamoRecord2, "#"));
   }
 }
